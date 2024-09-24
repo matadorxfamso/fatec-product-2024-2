20cc0605cafa6e1dbd45849a2af20dcd961860d9
matadorxfamoso <matadorxfamoso@gmail.com>
2024-09-24 11:28:12 -0300

    finalização aula save button

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -12,10 +12,19 @@ export class ProductService {
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>('http://localhost:3000/products');
   }
+  getProductsByid(id:number): Observable<Product> {
+    return this.http.get<Product>('http://localhost:3000/products/' + id);
+  }
 
   delete(Product: Product): Observable<void> {
     return this.http.delete<void>(
       'http://localhost:3000/products/' + Product.id
     );
+  }
+
+  update(product: Product): Observable<Product>{
+    return this.http.put<Product>(
+      'http://localhost:3000/products/' + product.id,
+      product);
   }
 }
